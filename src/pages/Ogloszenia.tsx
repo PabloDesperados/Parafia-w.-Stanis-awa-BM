36653e87e49d1c0a016b6422caea3e12ef2acbed
PawelPlaskacz <pawel.plaskacz0601@gmail.com>
2026-07-12 13:34:27 +0200

    feat: ustawa kamilkowa i ogłoszenia z 5 lipca

--- src/pages/Ogloszenia.tsx
+++ src/pages/Ogloszenia.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PdfViewer from "../components/PdfViewer";
 import ogloszeniaPdf from "../pdf/ogloszenia.pdf";
+import ustawaPdf from "../pdf/ustawa.pdf";
 
 export const Ogloszenia: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ export const Ogloszenia: React.FC = () => {
       <p>Aktualne ogłoszenia można zobaczyć poniżej:</p>
 
       <PdfViewer fileUrl={ogloszeniaPdf} />
+      <PdfViewer fileUrl={ustawaPdf} />
     </section>
   );
 };
